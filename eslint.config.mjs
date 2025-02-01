c3be8bdaa526819e7c21a36c34eebfa83b79b906
PhDBui <congdoan6997@gmail.com>
2025-02-02 01:48:08 +0300

    feat: 🎸 Project & IDE setup (Shadcn, Prettier, eslint, husky)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,20 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path"
+import { fileURLToPath } from "url"
+import { FlatCompat } from "@eslint/eslintrc"
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended"
+import pluginQuery from "@tanstack/eslint-plugin-query"
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...pluginQuery.configs["flat/recommended"],
+  eslintPluginPrettierRecommended,
+]
 
-export default eslintConfig;
+export default eslintConfig
